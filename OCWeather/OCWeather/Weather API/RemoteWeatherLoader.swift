1359e0e95b15c792f3c5e7f1f0adfc40d6e84421
Kylta <Christophe.bugnon02@gmail.com>
2019-02-17 09:57:52 +0100

    Delivers invalidData error or non 200 HTTP response

--- OCWeather/OCWeather/Weather API/RemoteWeatherLoader.swift
+++ OCWeather/OCWeather/Weather API/RemoteWeatherLoader.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 public protocol HTTPClient {
-    func get(from url: URL, completion: @escaping (Error) -> Void)
+    func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void)
 }
 
 public final class RemoteWeatherLoader {
@@ -23,11 +23,16 @@ public final class RemoteWeatherLoader {
 
     public enum Error: Swift.Error {
         case connectivity
+        case invalidData
     }
 
-    public func load(completion: @escaping (Error) -> Void) {
-        client.get(from: url) { error in
-            completion(.connectivity)
+    public func load(completion: @escaping (Error?) -> Void) {
+        client.get(from: url) { error, response in
+            if response != nil {
+                completion(.invalidData)
+            } else {
+                completion(.connectivity)
+            }
         }
     }
 }
--- OCWeather/OCWeatherTests/RemoteWeatherLoaderTests.swift
+++ OCWeather/OCWeatherTests/RemoteWeatherLoaderTests.swift
@@ -40,12 +40,23 @@ class RemoteWeatherLoaderTests: XCTestCase {
         let (sut, client) = makeSUT()
 
         var capturedError = [RemoteWeatherLoader.Error]()
-        sut.load { capturedError.append($0)}
+        sut.load { capturedError.append($0!)}
 
         let clientError = NSError(domain: "test", code: 0)
         client.complete(with: clientError)
 
         XCTAssertEqual(capturedError, [.connectivity])
+    }
+
+    func test_load_deliversErrorOnNon200HTTPResponse() {
+        let (sut, client) = makeSUT()
+
+        var capturedError = [RemoteWeatherLoader.Error]()
+        sut.load { capturedError.append($0!)}
+
+        client.complete(withStatusCode: 400)
+
+        XCTAssertEqual(capturedError, [.invalidData])
     }
 
     // MARK: - Helpers
@@ -57,18 +68,28 @@ class RemoteWeatherLoaderTests: XCTestCase {
     }
 
     class HTTPClientSpy: HTTPClient {
-        var messages = [(url: URL, completion: (Error) -> Void)]()
+        var messages = [(url: URL, completion: (Error?, HTTPURLResponse?) -> Void)]()
 
         var requestedURLs: [URL] {
             return messages.map { $0.url }
         }
 
-        func get(from url: URL, completion: @escaping (Error) -> Void) {
+        func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
             messages.append((url, completion))
         }
 
         func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(error)
+            messages[index].completion(error, nil)
+        }
+
+        func complete(withStatusCode code: Int, at index: Int = 0) {
+            let response = HTTPURLResponse(
+                url: requestedURLs[index],
+                statusCode: code,
+                httpVersion: nil,
+                headerFields: nil)!
+
+            messages[index].completion(nil, response)
         }
     }
 }
