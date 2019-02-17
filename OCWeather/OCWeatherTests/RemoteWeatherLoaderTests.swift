9e08038f2b55ebeef83971421f358f0956d0508f
Kylta <Christophe.bugnon02@gmail.com>
2019-02-17 09:08:51 +0100

    `RemoteWeatherLoader` does not request data upon creation

--- OCWeather/OCWeatherTests/RemoteWeatherLoaderTests.swift
+++ OCWeather/OCWeatherTests/RemoteWeatherLoaderTests.swift
@@ -6,4 +6,34 @@
 //  Copyright © 2019 Christophe Bugnon. All rights reserved.
 //
 
-import Foundation
+import XCTest
+
+class RemoteWeatherLoader {
+    let client: HTTPClient
+
+    init(client: HTTPClient) {
+        self.client = client
+    }
+}
+
+protocol HTTPClient {
+    func get(from url: URL)
+}
+
+class HTTPClientSpy: HTTPClient {
+    var requestedURL: URL?
+
+    func get(from url: URL) {
+        requestedURL = url
+    }
+}
+
+class RemoteWeatherLoaderTests: XCTestCase {
+
+    func test_init() {
+        let client = HTTPClientSpy()
+        _ = RemoteWeatherLoader(client: client)
+
+        XCTAssertNil(client.requestedURL)
+    }
+}
