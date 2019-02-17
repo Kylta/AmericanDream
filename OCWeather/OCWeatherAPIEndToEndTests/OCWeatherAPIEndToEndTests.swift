9d47655e4528a02e112c1bf2261b343915664fbe
Kylta <Christophe.bugnon02@gmail.com>
2019-02-17 17:16:08 +0100

    Extract test setup to a helper method

--- OCWeather/OCWeatherAPIEndToEndTests/OCWeatherAPIEndToEndTests.swift
+++ OCWeather/OCWeatherAPIEndToEndTests/OCWeatherAPIEndToEndTests.swift
@@ -12,6 +12,17 @@ import OCWeather
 class OCWeatherAPIEndToEndTests: XCTestCase {
 
     func test_endToEndTestServerGETWeatherResult_matchesFixedTestAccountData() {
+        switch getWeatherResult() {
+        case let .success(item)?:
+            XCTAssertEqual(item.name, "Paris")
+        case let .failure(error)?:
+            XCTFail("Expected successful weather result, got \(error) instead")
+        default:
+            XCTFail("Expected successful weather result, got no result instead")
+        }
+    }
+
+    private func getWeatherResult() -> LoadWeatherResult? {
         let client = URLSessionHTTPClient()
         let testServerURL = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=Paris,fr&units=metric&APPID=f33ca45f1e944339541f316aef6cda60")!
         let loader = RemoteWeatherLoader(client: client, url: testServerURL)
@@ -25,14 +36,6 @@ class OCWeatherAPIEndToEndTests: XCTestCase {
         }
 
         wait(for: [exp], timeout: 5.0)
-
-        switch receivedResult {
-        case let .success(item)?:
-            XCTAssertEqual(item.name, "Paris")
-        case let .failure(error)?:
-            XCTFail("Expected successful weather result, got \(error) instead")
-        default:
-            XCTFail("Expected successful weather result, got no result instead")
-        }
+        return receivedResult
     }
 }
