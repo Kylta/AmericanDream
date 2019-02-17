b97951b2ff09ad3e71e5c3d1465125126c1b7c96
Kylta <Christophe.bugnon02@gmail.com>
2019-02-17 14:31:24 +0100

    Add nil data and nil response to the request error test case to make the setup more explicit

--- OCWeather/OCWeatherTests/Weather API/URLSessionHTTPClientTests.swift
+++ OCWeather/OCWeatherTests/Weather API/URLSessionHTTPClientTests.swift
@@ -31,7 +31,7 @@ class URLSessionHTTPClientTests: XCTestCase {
         URLProtocolStub.startInterceptingRequests()
         let url = URL(string: "http://any-url.com")!
         let error = NSError(domain: "any error", code: 0)
-        URLProtocolStub.stub(url: url, error: error)
+        URLProtocolStub.stub(url: url, data: nil, response: nil, error: error)
 
         let sut = URLSessionHTTPClient()
 
@@ -58,11 +58,13 @@ class URLSessionHTTPClientTests: XCTestCase {
         private static var stubs = [URL: Stub]()
 
         private struct Stub {
+            let data: Data?
+            let response: URLResponse?
             let error: Error?
         }
 
-        static func stub(url: URL, error: NSError? = nil) {
-            URLProtocolStub.stubs[url] = Stub(error: error)
+        static func stub(url: URL, data: Data?, response: URLResponse?, error: NSError?) {
+            URLProtocolStub.stubs[url] = Stub(data: data, response: response, error: error)
         }
 
         static func startInterceptingRequests() {
@@ -89,6 +91,14 @@ class URLSessionHTTPClientTests: XCTestCase {
 
             if let error = stub.error {
                 client?.urlProtocol(self, didFailWithError: error)
+            }
+
+            if let response = stub.response {
+                client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            }
+
+            if let data = stub.data {
+                client?.urlProtocol(self, didLoad: data)
             }
 
             client?.urlProtocolDidFinishLoading(self)
