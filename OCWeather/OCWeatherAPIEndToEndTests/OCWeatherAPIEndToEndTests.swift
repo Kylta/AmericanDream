322047c99f63c117a3c4ae79016a56bac0ccae58
Kylta <Christophe.bugnon02@gmail.com>
2019-02-17 18:52:37 +0100

    Use sample URL test for end-to-end test

--- OCWeather/OCWeatherAPIEndToEndTests/OCWeatherAPIEndToEndTests.swift
+++ OCWeather/OCWeatherAPIEndToEndTests/OCWeatherAPIEndToEndTests.swift
@@ -14,7 +14,12 @@ class OCWeatherAPIEndToEndTests: XCTestCase {
     func test_endToEndTestServerGETWeatherResult_matchesFixedTestAccountData() {
         switch getWeatherResult() {
         case let .success(item)?:
-            XCTAssertEqual(item.name, "Paris")
+            XCTAssertEqual(item.name, "London")
+            XCTAssertEqual(item.temperature, 280.32)
+            XCTAssertEqual(item.date, 1485789600)
+            XCTAssertEqual(item.wind, 4.1)
+            XCTAssertEqual(item.weather, "Drizzle")
+            XCTAssertEqual(item.description, "light intensity drizzle")
         case let .failure(error)?:
             XCTFail("Expected successful weather result, got \(error) instead")
         default:
@@ -26,7 +31,7 @@ class OCWeatherAPIEndToEndTests: XCTestCase {
 
     private func getWeatherResult(file: StaticString = #file, line: UInt = #line) -> LoadWeatherResult? {
         let client = URLSessionHTTPClient()
-        let testServerURL = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=Paris,fr&units=metric&APPID=f33ca45f1e944339541f316aef6cda60")!
+        let testServerURL = URL(string: "https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22")!
         let loader = RemoteWeatherLoader(client: client, url: testServerURL)
         trackMemoryLeaks(instance: client, file: file, line: line)
         trackMemoryLeaks(instance: loader, file: file, line: line)
