5690ccbf347634f91d1eddd8e3aecf838f568007
Kylta <Christophe.bugnon02@gmail.com>
2019-02-20 13:58:50 +0100

    Import OCExchangeiOS project

---
+++ OCExchangeiOS/OCExchangeiOS/OCExchangeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OCExchangeiOS
+//
+//  Created by Christophe Bugnon on 20/02/2019.
+//  Copyright © 2019 Christophe Bugnon. All rights reserved.
+//
+
+import UIKit
+
+class OCExchangeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
